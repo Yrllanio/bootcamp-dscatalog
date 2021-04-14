0edebeff468f418f1b0d56f0eb1c03d42d108836
yrllanio <yrlanio@gmail.com>
2021-04-14 17:37:07 -0300

    Add toast when save product

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,3 +1,5 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './core/assets/styles/custom.scss';
 import './app.scss';
 import Routes from './Routes';
@@ -6,7 +8,10 @@ import Routes from './Routes';
 
 const App = () => {    
     return(
+      <>
+      <ToastContainer />
       <Routes />
+      </>      
     );
 }
 
--- front-web/src/pages/Admin/components/Products/Form/index.tsx
+++ front-web/src/pages/Admin/components/Products/Form/index.tsx
@@ -1,7 +1,9 @@
+import { useForm } from 'react-hook-form';
+import { toast } from 'react-toastify';
 import { makePrivateRequest } from 'core/utils/request';
 import BaseForm from '../../BaseForm';
 import './styles.scss';
-import { useForm } from 'react-hook-form';
+import { useHistory } from 'react-router-dom';
 
 type FormState = {
     name: string;
@@ -12,81 +14,90 @@ type FormState = {
 
 const Form = () => {
     const { register, handleSubmit, errors } = useForm<FormState>();
+    const history = useHistory();
 
     const onSubmit = (data: FormState) => {    
-        makePrivateRequest({ url: '/products', method: 'POST', data})       
+        makePrivateRequest({ url: '/products', method: 'POST', data})
+            .then(() => {
+                toast.info('Produto salvo com sucesso!');
+                history.push('/admin/products');
+            })
+            .catch(() => {
+                toast.error('Erro ao salvar produto!');
+            })       
     }
-return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-        <BaseForm title="cadastrar um produto">
-            <div className="row">
-                <div className="col-6">
-                    <div className="margin-bottom-30">
-                    <input
-                        ref={register({
-                            required: "Campo obrigatório",
-                            minLength: { value: 5, message: 'O campo deve ter no mínimo 5 caracteres'},
-                            maxLength: { value: 60, message: 'O campo deve ter no máximo 60 caracteres'}                        
-                        
-                        })}
-                        name="name"
-                        type="text"
-                        className="form-control input-base"                        
-                        placeholder="Nome do produto"
-                    /> 
-                    {errors.name && (
-                        <div className="invalid-feedback d-block">
-                            {errors.name.message}
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <BaseForm title="cadastrar um produto">
+                <div className="row">
+                    <div className="col-6">
+                        <div className="margin-bottom-30">
+                        <input
+                            ref={register({
+                                required: "Campo obrigatório",
+                                minLength: { value: 5, message: 'O campo deve ter no mínimo 5 caracteres'},
+                                maxLength: { value: 60, message: 'O campo deve ter no máximo 60 caracteres'}                        
+                            
+                            })}
+                            name="name"
+                            type="text"
+                            className="form-control input-base"                        
+                            placeholder="Nome do produto"
+                        /> 
+                        {errors.name && (
+                            <div className="invalid-feedback d-block">
+                                {errors.name.message}
+                            </div>
+                        )}
+                        </div>                 
+                        <div className="margin-bottom-30">
+                        <input
+                            ref={register({ required: "Campo obrigatório"})}
+                            name="price"
+                            type="number"
+                            className="form-control input-base"                        
+                            placeholder="Preço"
+                        />
+                        {errors.price && (
+                            <div className="invalid-feedback d-block">
+                                {errors.price.message}
+                            </div>
+                        )}
                         </div>
-                    )}
-                    </div>                 
-                    <div className="margin-bottom-30">
-                    <input
-                        ref={register({ required: "Campo obrigatório"})}
-                        name="price"
-                        type="number"
-                        className="form-control input-base"                        
-                        placeholder="Preço"
-                    />
-                    {errors.price && (
-                        <div className="invalid-feedback d-block">
-                            {errors.price.message}
+                        <div className="margin-bottom-30">
+                        <input
+                            ref={register({ required: "Campo obrigatório"})}
+                            name="imageUrl"
+                            type="text"
+                            className="form-control input-base"
+                            placeholder="Imagem do produto"
+                        />
+                        {errors.imageUrl && (
+                            <div className="invalid-feedback d-block">
+                                {errors.imageUrl.message}
+                            </div>
+                        )}
                         </div>
-                    )}
                     </div>
-                    <div className="margin-bottom-30">
-                    <input
-                        ref={register({ required: "Campo obrigatório"})}
-                        name="imageUrl"
-                        type="text"
-                        className="form-control input-base"
-                        placeholder="Imagem do produto"
-                    />
-                    {errors.imageUrl && (
-                        <div className="invalid-feedback d-block">
-                            {errors.imageUrl.message}
-                        </div>
-                    )}
+                    <div className="col-6">
+                        <textarea
+                            ref={register({ required: "Campo obrigatório"})}
+                            name="description"
+                            className="form-control input-base"
+                            placeholder="Descrição"
+                            cols={30}
+                            rows={10}
+                        />
+                        {errors.description && (
+                            <div className="invalid-feedback d-block">
+                                {errors.description.message}
+                            </div>
+                        )}
                     </div>
                 </div>
-                <div className="col-6">
-                    <textarea
-                        ref={register({ required: "Campo obrigatório"})}
-                        name="description"
-                        className="form-control input-base"
-                        placeholder="Descrição"
-                        cols={30}
-                        rows={10}
-                    />
-                    {errors.description && (
-                        <div className="invalid-feedback d-block">
-                            {errors.description.message}
-                        </div>
-                    )}
-                </div>
-            </div>
-        </BaseForm>
-    </form>
-)}
+            </BaseForm>
+        </form>
+    )
+}
 
 export default Form;
